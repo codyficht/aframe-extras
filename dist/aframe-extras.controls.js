728533b8e96456c1cebdf6520b702c68bf563ad3
Cody <cody.ficht@icloud.com>
2021-08-21 13:06:42 -0400

    Update aframe-extras.controls.js

--- dist/aframe-extras.controls.js
+++ dist/aframe-extras.controls.js
@@ -1527,13 +1527,13 @@ module.exports = AFRAME.registerComponent('movement-controls', {
 
       if (data.constrainToNavMesh && velocityCtrl.isNavMeshConstrained !== false) {
 
-        if (velocity.lengthSq() < EPS) return;
+        if (velocity.lengthSq() < EPS || el.sceneEl.systems.nav == undefined) return;
 
         start.copy(el.object3D.position);
         end.copy(velocity).multiplyScalar(dt / 1000).add(start);
 
         var nav = el.sceneEl.systems.nav;
-        this.navGroup = this.navGroup;
+        this.navGroup = this.navGroup === null ? nav.getGroup(start) : this.navGroup;
         this.navNode = this.navNode || nav.getNode(start, this.navGroup);
         this.navNode = nav.clampStep(start, end, this.navGroup, this.navNode, clampedEnd);
         el.object3D.position.copy(clampedEnd);
